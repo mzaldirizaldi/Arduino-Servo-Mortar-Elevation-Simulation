46c40a47bdb5ae3156f76fd2e1cf0cda72fd5f7d
mzaldirizaldi <mzaldirizaldi@gmail.com>
2022-09-08 17:24:07 +0700

    Add files via upload

---
+++ Mortar Sim Servo/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Mortar_Sim_Servo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox_comPortSettings = new System.Windows.Forms.GroupBox();
+            this.progressBar_uno = new System.Windows.Forms.ProgressBar();
+            this.button_unoClose = new System.Windows.Forms.Button();
+            this.button_unoOpen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_unoBaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBox_unoComPort = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_degree = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.textBox_distance = new System.Windows.Forms.TextBox();
+            this.textBox_ammo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort_uno = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox_comPortSettings.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_comPortSettings
+            // 
+            this.groupBox_comPortSettings.Controls.Add(this.progressBar_uno);
+            this.groupBox_comPortSettings.Controls.Add(this.button_unoClose);
+            this.groupBox_comPortSettings.Controls.Add(this.button_unoOpen);
+            this.groupBox_comPortSettings.Controls.Add(this.label2);
+            this.groupBox_comPortSettings.Controls.Add(this.label1);
+            this.groupBox_comPortSettings.Controls.Add(this.comboBox_unoBaudRate);
+            this.groupBox_comPortSettings.Controls.Add(this.comboBox_unoComPort);
+            this.groupBox_comPortSettings.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_comPortSettings.Name = "groupBox_comPortSettings";
+            this.groupBox_comPortSettings.Size = new System.Drawing.Size(329, 216);
+            this.groupBox_comPortSettings.TabIndex = 1;
+            this.groupBox_comPortSettings.TabStop = false;
+            this.groupBox_comPortSettings.Text = "COM PORT SETTINGS";
+            // 
+            // progressBar_uno
+            // 
+            this.progressBar_uno.Location = new System.Drawing.Point(6, 180);
+            this.progressBar_uno.Name = "progressBar_uno";
+            this.progressBar_uno.Size = new System.Drawing.Size(317, 23);
+            this.progressBar_uno.TabIndex = 6;
+            // 
+            // button_unoClose
+            // 
+            this.button_unoClose.Location = new System.Drawing.Point(166, 129);
+            this.button_unoClose.Name = "button_unoClose";
+            this.button_unoClose.Size = new System.Drawing.Size(114, 39);
+            this.button_unoClose.TabIndex = 5;
+            this.button_unoClose.Text = "CLOSE";
+            this.button_unoClose.UseVisualStyleBackColor = true;
+            this.button_unoClose.Click += new System.EventHandler(this.button_unoClose_Click);
+            // 
+            // button_unoOpen
+            // 
+            this.button_unoOpen.Location = new System.Drawing.Point(35, 129);
+            this.button_unoOpen.Name = "button_unoOpen";
+            this.button_unoOpen.Size = new System.Drawing.Size(114, 39);
+            this.button_unoOpen.TabIndex = 4;
+            this.button_unoOpen.Text = "OPEN";
+            this.button_unoOpen.UseVisualStyleBackColor = true;
+            this.button_unoOpen.Click += new System.EventHandler(this.button_unoOpen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BAUD RATE:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM PORT:";
+            // 
+            // comboBox_unoBaudRate
+            // 
+            this.comboBox_unoBaudRate.FormattingEnabled = true;
+            this.comboBox_unoBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox_unoBaudRate.Location = new System.Drawing.Point(135, 91);
+            this.comboBox_unoBaudRate.Name = "comboBox_unoBaudRate";
+            this.comboBox_unoBaudRate.Size = new System.Drawing.Size(145, 24);
+            this.comboBox_unoBaudRate.TabIndex = 1;
+            // 
+            // comboBox_unoComPort
+            // 
+            this.comboBox_unoComPort.FormattingEnabled = true;
+            this.comboBox_unoComPort.Location = new System.Drawing.Point(135, 49);
+            this.comboBox_unoComPort.Name = "comboBox_unoComPort";
+            this.comboBox_unoComPort.Size = new System.Drawing.Size(145, 24);
+            this.comboBox_unoComPort.TabIndex = 0;
+            this.comboBox_unoComPort.DropDown += new System.EventHandler(this.comboBox_unoComPort_DropDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_degree);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button_send);
+            this.groupBox1.Controls.Add(this.textBox_distance);
+            this.groupBox1.Controls.Add(this.textBox_ammo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(347, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 216);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ELEVATION CONTROL";
+            // 
+            // label_degree
+            // 
+            this.label_degree.AutoSize = true;
+            this.label_degree.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_degree.Location = new System.Drawing.Point(211, 166);
+            this.label_degree.Name = "label_degree";
+            this.label_degree.Size = new System.Drawing.Size(0, 26);
+            this.label_degree.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(159, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ELEVATION DEGREE:";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(324, 59);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(75, 36);
+            this.button_send.TabIndex = 4;
+            this.button_send.Text = "SEND";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // textBox_distance
+            // 
+            this.textBox_distance.Location = new System.Drawing.Point(164, 88);
+            this.textBox_distance.Name = "textBox_distance";
+            this.textBox_distance.Size = new System.Drawing.Size(133, 22);
+            this.textBox_distance.TabIndex = 3;
+            // 
+            // textBox_ammo
+            // 
+            this.textBox_ammo.Location = new System.Drawing.Point(164, 48);
+            this.textBox_ammo.Name = "textBox_ammo";
+            this.textBox_ammo.Size = new System.Drawing.Size(133, 22);
+            this.textBox_ammo.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "DISTANCE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "AMMUNITION:";
+            // 
+            // serialPort_uno
+            // 
+            this.serialPort_uno.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_uno_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 240);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox_comPortSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "MORTAR SERVO SIM";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox_comPortSettings.ResumeLayout(false);
+            this.groupBox_comPortSettings.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_comPortSettings;
+        private System.Windows.Forms.ProgressBar progressBar_uno;
+        private System.Windows.Forms.Button button_unoClose;
+        private System.Windows.Forms.Button button_unoOpen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_unoBaudRate;
+        private System.Windows.Forms.ComboBox comboBox_unoComPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.TextBox textBox_distance;
+        private System.Windows.Forms.TextBox textBox_ammo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort_uno;
+        private System.Windows.Forms.Label label_degree;
+    }
+}
+
---
+++ Mortar Sim Servo/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mortar_Sim_Servo
+{
+
+    public partial class Form1 : Form
+    {
+        string unoDataIn, degree;
+        sbyte indexOfA;
+
+        string[] isian0 = { "100", "150", "200", "250", "300", "350", "400", "450" };
+        string[] isian1 = { "200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800" };
+        string[] isian2 = { "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900", "950",
+                      "1000", "1050", "1100", "1150", "1200", "1250", "1300", "1350" };
+        string[] isian3 = { "600", "700", "800", "900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800",
+                      "1900", "2000", "2100", "2200", "2300", "2400", "2500", "2600" };
+        string[] isian4 = { "1900", "2000", "2100", "2200", "2300", "2400", "2500", "2600", "2700", "2800", "2900", "3000",
+                      "3100", "3200", "3300", "3400", "3500", "3600", "3700" };
+        string[] isian5 = { "2400", "2500", "2600", "2700", "2800", "2900", "3000", "3100", "3200", "3300", "3400", "3500",
+                      "3600", "3700", "3800", "3900", "4000", "4100", "4200", "4300", "4400", "4500", "4600", "4700" };
+        string[] isian6 = { "3500", "3600", "3700", "3800", "3900", "4000", "4100", "4200", "4300", "4400", "4500", "4600",
+                      "4700", "4800", "4900", "5000", "5100", "5200", "5300", "5400", "5500", "5600" };
+        string[] isian7 = { "3900", "4000", "4100", "4200", "4300", "4400", "4500", "4600", "4700", "4800", "4900", "5000",
+                      "5100", "5200", "5300", "5400", "5500", "5600", "5700", "5800", "5900", "6000", "6100", "6200", "6300" };
+        string[] isian8 = { "4500", "4600", "4700", "4800", "4900", "5000", "5100", "5200", "5300", "5400", "5500", "5600",
+                      "5700", "5800", "5900", "6000", "6100", "6200", "6300", "6400", "6500" };
+
+        int[] sudut0 = { 83, 80, 76, 73, 69, 64, 58, 45 };
+        int[] sudut1 = { 82, 80, 78, 77, 74, 72, 70, 68, 65, 62, 59, 55, 45 };
+        int[] sudut2 = { 83, 82, 81, 80, 79, 77, 76, 75, 74, 73, 71, 70, 68, 67, 65, 64, 62, 60, 68, 55, 52, 45 };
+        int[] sudut3 = { 83, 82, 81, 79, 78, 77, 76, 75, 73, 72, 71, 69, 68, 66, 64, 63, 61, 58, 56, 53, 46 };
+        int[] sudut4 = { 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 62, 61, 60, 58, 56, 54, 51, 46 };
+        int[] sudut5 = { 74, 73, 72, 72, 71, 70, 69, 68, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 57, 56, 54, 52, 49, 46 };
+        int[] sudut6 = { 69, 69, 68, 67, 67, 66, 65, 64, 63, 63, 62, 61, 60, 59, 58, 57, 55, 54, 52, 50, 47, 45 };
+        int[] sudut7 = { 69, 69, 68, 68, 67, 66, 66, 65, 64, 64, 63, 62, 61, 60, 60, 59, 58, 57, 55, 54, 53, 51, 49, 47, 45 };
+        int[] sudut8 = { 68, 67, 66, 66, 65, 64, 64, 63, 62, 61, 61, 60, 59, 58, 57, 56, 55, 53, 52, 50, 45 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_unoComPort_DropDown(object sender, EventArgs e)
+        {
+            string[] portLists = SerialPort.GetPortNames();
+            comboBox_unoComPort.Items.Clear();
+            comboBox_unoComPort.Items.AddRange(portLists);
+        }
+
+        private void button_unoOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort_uno.PortName = comboBox_unoComPort.Text;
+                serialPort_uno.BaudRate = Convert.ToInt32(comboBox_unoBaudRate.Text);
+                serialPort_uno.Open();
+
+                button_unoOpen.Enabled = false;
+                button_unoClose.Enabled = true;
+                progressBar_uno.Value = 100;
+                serialPort_uno.Write("45" + "#" + "\n");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void button_unoClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort_uno.Write("45" + "#" + "\n");
+                serialPort_uno.Close();
+                button_unoOpen.Enabled = true;
+                button_unoClose.Enabled = false;
+                progressBar_uno.Value = 0;
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox_ammo.Text == "0")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian0.findIndex(distance);
+                int y = sudut0[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "1")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian1.findIndex(distance);
+                int y = sudut1[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "2")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian2.findIndex(distance);
+                int y = sudut2[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "3")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian3.findIndex(distance);
+                int y = sudut3[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "4")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian4.findIndex(distance);
+                int y = sudut4[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "5")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian5.findIndex(distance);
+                int y = sudut5[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "6")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian6.findIndex(distance);
+                int y = sudut6[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "7")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian7.findIndex(distance);
+                int y = sudut7[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+            if (textBox_ammo.Text == "8")
+            {
+                string distance = textBox_distance.Text;
+                int x = isian8.findIndex(distance);
+                int y = sudut8[x];
+                label_degree.Text = Convert.ToString(y) + "°";
+                serialPort_uno.Write(y + "#" + "\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_unoOpen.Enabled = true;
+            button_unoClose.Enabled = false;
+            progressBar_uno.Value = 0;
+            comboBox_unoBaudRate.Text = "9600";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort_uno.Write("45" + "#" + "\n");
+                serialPort_uno.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void serialPort_uno_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            unoDataIn = serialPort_uno.ReadLine();
+            this.BeginInvoke(new EventHandler(ProcessData));
+        }
+
+        private void ProcessData(object sender, EventArgs e)
+        {
+            try
+            {
+                indexOfA = Convert.ToSByte(unoDataIn.IndexOf("A"));
+                degree = unoDataIn.Substring(0, indexOfA);
+                label_degree.Text = String.Format(degree + "°");
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+    public static class Extensions
+    {
+        public static int findIndex<T>(this T[] isian0, T stringtoFind)
+        {
+            return Array.IndexOf(isian0, stringtoFind);
+        }
+    }
+}
